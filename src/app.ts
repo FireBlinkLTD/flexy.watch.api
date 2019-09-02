78d98917b1ee4cf7f853a2c0b607e5b6029c4804
igor.zinchenko <igor.zinchenko557@gmail.com>
2019-09-02 15:54:40 +0300

    feat: integrated TypeORM

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import {GlobalAcceptMimesMiddleware, ServerLoader, ServerSettings} from "@tsed/common";
 import * as bodyParser from "body-parser";
+import * as config from "config";
 import {IndexController} from "./controllers";
 import RequestLoggerMiddleware from "./middlewares/request-logger";
+import {Client} from "pg";
+import {DatabaseLogger} from "./lib/database-logger";
+
+const dbConfig = config.get<{url: string, schema: string}>("db");
 
 @ServerSettings({
   acceptMimes: ["application/json"],
@@ -19,11 +24,28 @@ import RequestLoggerMiddleware from "./middlewares/request-logger";
     logRequest: false,
     disableRoutesSummary: true
   },
+  typeorm: [
+    {
+      name: "default",
+      type: "postgres",
+      url: dbConfig.url,
+      schema: dbConfig.schema,
+      migrationsRun: true,
+      synchronize: false,
+      logger: new DatabaseLogger(),
+      entities: [
+        `${__dirname}/entity/*.js`
+      ],
+      migrations: [
+        `${__dirname}/migrations/*.js`
+      ]
+    }
+  ],
   httpPort: 3000
 })
 export class App extends ServerLoader {
 
-  public $onMountingMiddlewares(): void|Promise<any> {
+  public $onMountingMiddlewares(): void | Promise<any> {
     this
       .use(GlobalAcceptMimesMiddleware)
       .use(RequestLoggerMiddleware)
@@ -33,5 +55,12 @@ export class App extends ServerLoader {
       }));
 
     return null;
+  }
+
+  public async $onInit(): Promise<any> {
+    const client = new Client({connectionString: dbConfig.url});
+    await client.connect();
+    await client.query(`CREATE SCHEMA IF NOT EXISTS "${dbConfig.schema}"`);
+    await client.end();
   }
 }
